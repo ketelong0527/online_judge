9b731ac5db3c7fa34b9a732b3fd254a39cd78c68
ketelong0527 <ketelong0527@github.com>
2026-06-21 21:15:42 +0800

    Add MEDIA configuration for ImageField

--- online_judge/settings.py
+++ online_judge/settings.py
@@ -92,6 +92,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = "/problems/"
 LOGOUT_REDIRECT_URL = "/problems/"
