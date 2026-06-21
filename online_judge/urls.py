432a4c2427b8e7415525dff40f78e5814a39aff3
ketelong0527 <ketelong0527@github.com>
2026-06-21 21:11:46 +0800

    Add root path redirect to problems

--- online_judge/urls.py
+++ online_judge/urls.py
@@ -4,8 +4,10 @@ URL configuration for online_judge project.
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/problems/", permanent=True)),
     path("admin/", admin.site.urls),
     path("problems/", include('oj_problems.urls')),
     path("users/", include('oj_users.urls')),
